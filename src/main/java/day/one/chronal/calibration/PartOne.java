c229d627d667d10221d2c1be87c7f5587f4dd961
Paul Queruel <paul.queruel@viacesi.fr>
2018-12-02 13:26:27 +0100

    Day 1 Part 1

---
+++ src/main/java/day/one/chronal/calibration/PartOne.java
@@ -0,0 +1,18 @@
+package day.one.chronal.calibration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class PartOne
+{
+    public static void main(String [ ] args) throws IOException
+    {
+        System.out.println(String.format("The answer is %s",
+                Files.readAllLines(Paths.get("day-one.data"))
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .sum())
+        );
+    }
+}
---
+++ src/main/java/day/one/chronal/calibration/PartTwo.java
@@ -0,0 +1,16 @@
+package day.one.chronal.calibration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PartTwo
+{
+    public static void main(String [ ] args) throws IOException
+    {
+        List<String> input = Files.readAllLines(Paths.get("day-one.data"));
+
+        
+    }
+}
