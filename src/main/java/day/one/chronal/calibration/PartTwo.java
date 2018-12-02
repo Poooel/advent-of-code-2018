ae28d54f675640d2ad303c766d4ef2d297fbbd84
Paul Queruel <paul.queruel@viacesi.fr>
2018-12-02 13:35:05 +0100

    Day 1 Part 2

--- src/main/java/day/one/chronal/calibration/PartTwo.java
+++ src/main/java/day/one/chronal/calibration/PartTwo.java
@@ -1,16 +1,29 @@
 package day.one.chronal.calibration;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PartTwo
 {
-    public static void main(String [ ] args) throws IOException
+    public static void main(String [ ] args) throws Exception
     {
         List<String> input = Files.readAllLines(Paths.get("day-one.data"));
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        int frequency = 0;
 
-        
+        while (true) {
+            for (String number : input) {
+                frequency += Integer.parseInt(number);
+
+                if (frequencies.containsKey(frequency)) {
+                    throw new Exception(String.format("The answer is %s", frequency));
+                }
+
+                frequencies.put(frequency, frequency);
+            }
+        }
     }
 }
